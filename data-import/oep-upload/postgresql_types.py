3047234575997f387b956e10181591ad4c0a3cd7
jh-RLI <jonas.huber@rl-institut.de>
2020-05-06 18:46:33 +0200

    add oem 2 orm functionality, add postgis types, add folder as oem source

---
+++ data-import/oep-upload/postgresql_types.py
@@ -0,0 +1,40 @@
+__copyright__ = "Reiner Lemoine Institut"
+__license__   = "GNU Affero General Public License Version 3 (AGPL-3.0)"
+__url__       = "https://github.com/openego/data_processing/blob/master/LICENSE"
+__author__    = "henhuy"
+
+import sqlalchemy as sa
+import sqlalchemy.dialects.postgresql as psql
+from geoalchemy2 import Geometry
+
+
+class DatabaseTypes:
+    types = {
+        "bigint": sa.BIGINT,
+        "int": sa.INTEGER,
+        "integer": sa.INTEGER,
+        "double precision": sa.ARRAY(DOUBLE_PRECISION),
+            # psql.DOUBLE_PRECISION,
+        "varchar": sa.VARCHAR,
+        "json": sa.JSON,
+        "text": sa.TEXT,
+        "geometry point": Geometry("POINT"),
+        "timestamp": sa.TIMESTAMP,
+        "interval": sa.Interval,
+        "string": sa.String,
+        "float": sa.FLOAT,
+        "boolean": sa.Boolean,
+        "date": sa.Date,
+        "geom": Geometry("GEOMETRY"),
+        "geometry": Geometry("GEOMETRY")
+    }
+
+    def __getitem__(self, item):
+        if item.split(" ")[-1] == "array":
+            db_type = self.types[item[:-6]]
+            return psql.ARRAY(db_type)
+        else:
+            return self.types[item]
+
+
+TYPES = DatabaseTypes()
