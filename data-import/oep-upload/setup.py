3c1fc10d20828aa2b483fee92ef859380b85c400
jh-RLI <jonas.huber@rl-institut.de>
2020-05-27 17:12:06 +0200

    update setup.py

--- data-import/oep-upload/setup.py
+++ data-import/oep-upload/setup.py
@@ -3,7 +3,7 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, "..", "README.md"), encoding="utf-8") as fh:
+with open(path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -11,7 +11,7 @@ setuptools.setup(
     version="0.1.0",
     author="henhuy, jh-RLI",
     author_email="Hendrik.Huyskens@rl-institut.de",
-    description="SQLAlchemy module to create ORM, read from data model in open-energy-metadata(oem-v1.4.0) JSON format",
+    description="SQLAlchemy module to generate ORM, read from data model in open-energy-metadata(oem-v1.4.0) JSON format",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/OpenEnergyPlatform/data-preprocessing/tree/feature/oep-upload-oem2orm/data-import/oep-upload",
@@ -24,11 +24,11 @@ setuptools.setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: AGPL-3.0 License",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires=['sqlalchemy'],  # Optional
+    install_requires=['sqlalchemy', 'oedialect', 'jmespath'],  # Optional
     project_urls={  # Optional
         'Bug Reports': 'https://github.com/OpenEnergyPlatform/data-preprocessing/issues',
         'Source': 'https://github.com/OpenEnergyPlatform/data-preprocessing/tree/feature/oep-upload-oem2orm/data-import/oep-upload/oem2orm',
