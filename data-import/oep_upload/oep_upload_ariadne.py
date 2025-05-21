75c3e0b5a04528ad8f6b0b58e9d59561ab751381
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2025-05-21 22:22:14 +0200

    Use OMI to infer metadata and field #116

--- data-import/oep_upload/oep_upload_ariadne.py
+++ data-import/oep_upload/oep_upload_ariadne.py
@@ -1,26 +1,66 @@
 
+import json
+from pathlib import Path
+
 from frictionless.resources import TableResource
 from frictionless import Schema, fields
+from omi.inspection import infer_metadata
+
+def get_file_path_csv(fn):
+    # Get path
+    path_fn_csv = Path(__file__).parent / "data" / f"{fn}.csv"
+    print(path_fn_csv)
+
+    return path_fn_csv
+
+def get_file_path_json(fn):
+    # Get path
+    path_fn_json = Path(__file__).parent / "data" / f"{fn}.json"
+    print(path_fn_json)
+
+    return path_fn_json
 
 def frictionless_table_infer(fn):
+    # Get path
+    path_fn_csv = get_file_path_csv(fn)
+
+    # Infer metadata
     resource = TableResource(
-        path=fn)
+        path = path_fn_csv)
     resource.infer(stats=True)
+
     print(resource)
 
     return resource
 
+def omi_infer_metadata(fn):
 
-def frictionless_resource_fields(resource):
-    # https://framework.frictionlessdata.io/docs/framework/schema.html?query=fields
+    # Get path
+    path_fn_csv = get_file_path_csv(fn)
+    print(path_fn_csv)
+    path_fn_json = get_file_path_json(fn)
+    print(path_fn_json)
 
+    # Infer metadata from CSV file
+    with path_fn_csv.open("r") as f:
+        metadata = infer_metadata(f, "OEP")
+
+    # Save to a JSON file
+    with open(path_fn_json, "w",
+              encoding="utf-8") as json_file:
+        json.dump(metadata, json_file, ensure_ascii=False, indent=4)
+
+    print(metadata)
+
+    return metadata
 
 
 if __name__ == '__main__':
 
     # Inspect CSV
-    fn_data = 'data/2025-05-05_Ariadne2_Data_v1.0_data.csv'
-    resource = frictionless_table_infer(fn_data)
+    fn_data = '2025-05-05_Ariadne2_Data_v1.0_data'
+    # resource = frictionless_table_infer(fn_data)
+    metadata = omi_infer_metadata(fn_data)
 
     # Convert schema.field to
-    fields =
+    #fields =
