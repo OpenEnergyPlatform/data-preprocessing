e126c78efac26559157d7718d29b2766c3610152
Ludwig Hülk <ludwig.huelk@rl-institut.de>
2025-05-21 12:54:48 +0200

    Get fields #116

--- data-import/oep_upload/oep_upload_ariadne.py
+++ data-import/oep_upload/oep_upload_ariadne.py
@@ -1,15 +1,26 @@
 
 from frictionless.resources import TableResource
+from frictionless import Schema, fields
 
 def frictionless_table_infer(fn):
     resource = TableResource(
         path=fn)
     resource.infer(stats=True)
     print(resource)
+
     return resource
+
+
+def frictionless_resource_fields(resource):
+    # https://framework.frictionlessdata.io/docs/framework/schema.html?query=fields
+
+
 
 if __name__ == '__main__':
 
     # Inspect CSV
     fn_data = 'data/2025-05-05_Ariadne2_Data_v1.0_data.csv'
-    frictionless_table_infer(fn_data)
+    resource = frictionless_table_infer(fn_data)
+
+    # Convert schema.field to
+    fields =
